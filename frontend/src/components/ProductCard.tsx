344f4867fa9af59a5b0da6566b878462246dffae
yosko99 <thedriftmaniaka1997@gmail.com>
2023-01-06 12:01:21 +0200

    Add Material UI to frontend

---
+++ frontend/src/components/ProductCard.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+import { ProductType } from '../types/product.type';
+
+interface Props {
+  product: ProductType;
+}
+const ProductCard:React.FC<Props> = ({ product }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={product.image}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {product.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {product.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ProductCard;
--- frontend/src/views/MainPage.tsx
+++ frontend/src/views/MainPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import { useQuery } from '@apollo/client';
+import { Typography } from '@mui/material';
 
 import CreateProductForm from '../components/CreateProductForm';
-import Product from '../components/Product';
+import ProductCard from '../components/ProductCard';
 import { GET_PRODUCTS_QUERY } from '../queries/getProductsQuery';
 import { ProductType } from '../types/product.type';
 
@@ -18,12 +19,15 @@ const MainPage = () => {
     <div className='App-header'>
       <div className='container'>
         <CreateProductForm />
-
-        <div className='d-flex flex-wrap' >
+        <Typography align='center' fontSize={'1em'}>No data :(</Typography>
+        {data === undefined
+          ? <Typography align='center'>No data</Typography>
+          : <div className='d-flex flex-wrap' >
           {data.products.map((product: ProductType, index: number) => (
-            <Product key={index} product={product} />
+            <ProductCard key={index} product={product} />
           ))}
         </div>
+        }
       </div>
     </div>
   );
